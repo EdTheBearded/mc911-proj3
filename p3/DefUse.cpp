fbb147ae4d20aa9190e1bb215cd865f8d3dbc13e
Julia Ramos Beltrão <julia.ramos.beltrao@gmail.com>
2015-05-27 17:15:42 -0300

    Passe DefUse já é capaz de reconhecer definições morta e deletá-las.

--- p3/DefUse.cpp
+++ p3/DefUse.cpp
@@ -1,5 +1,9 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/IR/IntrinsicInst.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
@@ -10,6 +14,28 @@ namespace {
     DefUse() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
+	errs() << F.getName() << "\n";
+	inst_iterator I, E, aux;
+	for (I = inst_begin(F), E = inst_end(F); I != E; ++I){
+		if(!(I->getName().empty())){
+			if(I->hasNUses(0) && !(I->mayHaveSideEffects()) && !(I->isTerminator()) && !(isa<DbgInfoIntrinsic>(*I)) && !(isa<LandingPadInst>(*I))){
+			//if(I->hasNUses(0)){
+			errs() << I->getName() << " is used " << I->getNumUses() << " time(s)\n";
+			/*for (Use &U : pi->operands()) {
+  				Value *v = U.get();
+   			}*/
+			/*for (User *U : I->users()) {
+				if (Instruction *Inst = dyn_cast<Instruction>(U)) {
+					errs() << *Inst << "\n";
+				}
+			}*/
+			//aux = I;
+			//I++;
+			//aux->eraseFromParent();
+			errs() << "###########################################\n";
+		}
+		}
+	}
       	// W: uma lista de variaveis da função
 	// enquanto W não esta vazio
 		// remove uma variavel v de W
@@ -20,6 +46,7 @@ namespace {
 				// para cada variavel u usada por s
 					// delete s da lista de usos de u
 					// insira u em W  
+	
       return false;
     }
   };
